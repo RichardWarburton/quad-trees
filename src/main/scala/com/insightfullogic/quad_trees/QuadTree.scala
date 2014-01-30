744a0586c4afee9aef3448fd28e13e04b0f90b85
Richard Warburton <richard.warburton@gmail.com>
2014-01-30 23:45:04 +0000

    hmm

---
+++ src/main/scala/com/insightfullogic/quad_trees/QuadTree.scala
@@ -0,0 +1,9 @@
+package com.insightfullogic.quad_trees
+
+class QuadTree(boundary:Box) {
+
+}
+
+object QuadTree {
+  private val capacity = 4
+}
---
+++ src/test/scala/com/insightfullogic/quad_trees/UnitTest.scala
@@ -0,0 +1,7 @@
+package com.insightfullogic.quad_trees
+
+import org.scalatest.{Matchers, FlatSpec}
+
+abstract class UnitTest extends FlatSpec with Matchers {
+
+}
